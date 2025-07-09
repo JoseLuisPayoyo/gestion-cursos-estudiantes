f8779cf933186d87202000d820aff81a501723ce
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-09 14:03:35 +0200

    feat: implementar CursoMapper con métodos estáticos

---
+++ backend/src/main/java/com/payoyo/gestion_cursos/mapper/CursoMapper.java
@@ -0,0 +1,34 @@
+package com.payoyo.gestion_cursos.mapper;
+
+import com.payoyo.gestion_cursos.dto.CursoDTO;
+import com.payoyo.gestion_cursos.model.Curso;
+
+public class CursoMapper {
+
+    public static CursoDTO toDTO(Curso curso){
+        if (curso == null) return null;
+
+        CursoDTO cursoDTO = new CursoDTO();
+        
+        cursoDTO.setId(curso.getId());
+        cursoDTO.setNombre(curso.getNombre());
+        cursoDTO.setDescripcion(curso.getDescripcion());
+        cursoDTO.setDuracionHoras(curso.getDuracionHoras());
+
+        return cursoDTO;
+    }
+
+    public static Curso toEntity(CursoDTO cursoDTO){
+        if (cursoDTO == null) return null;
+
+        Curso curso = new Curso();
+
+        curso.setId(cursoDTO.getId());
+        curso.setNombre(cursoDTO.getNombre());
+        curso.setDescripcion(cursoDTO.getDescripcion());
+        curso.setDuracionHoras(cursoDTO.getDuracionHoras());
+
+        return curso;
+    }
+    
+}
