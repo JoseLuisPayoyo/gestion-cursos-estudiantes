c5ad180c7e668a6dbb9632f9144e5a4d923f45f7
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-09 13:46:10 +0200

    creado el modelo

---
+++ backend/src/main/java/com/payoyo/gestion_cursos/model/Curso.java
@@ -0,0 +1,28 @@
+package com.payoyo.gestion_cursos.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cursos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descripcion;
+    private int duracion;
+    
+}
