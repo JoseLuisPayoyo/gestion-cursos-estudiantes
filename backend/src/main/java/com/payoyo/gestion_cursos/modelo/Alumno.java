2f11222ac6509189b9cec5a821a48d6b98e12726
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-09 16:26:00 +0200

    fear: creada entidad alumno

---
+++ backend/src/main/java/com/payoyo/gestion_cursos/modelo/Alumno.java
@@ -0,0 +1,34 @@
+package com.payoyo.gestion_cursos.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "alumnos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Alumno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nombre;
+    private String correo;
+    private String telefono;
+
+    @ManyToOne
+    @JoinColumn(name = "curso_id", nullable = false)
+    private Curso curso;
+}
