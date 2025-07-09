d7a2601eb2162be34c6c08e692bad854dfd5a001
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-09 13:48:49 +0200

    creada la excepcion de recurso no encontrado

---
+++ backend/src/main/java/com/payoyo/gestion_cursos/exception/RecursoNoEncontradoException.java
@@ -0,0 +1,9 @@
+package com.payoyo.gestion_cursos.exception;
+
+public class RecursoNoEncontradoException extends RuntimeException{
+
+    public RecursoNoEncontradoException(String mensaje){
+        super(mensaje);
+    }
+    
+}
